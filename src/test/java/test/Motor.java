b016e5e8c244f458b6f55f6e99a055f26d955a9e
Juanfepx <juanfepx@gmail.com>
2023-08-21 20:38:54 -0500

    primer intento

---
+++ src/test/java/test/Motor.java
@@ -0,0 +1,14 @@
+package test;
+
+public class Motor {
+    int numeroCilindros;
+    String tipo;
+    int registro;
+
+    void cambiarRegistro(int registro){
+
+    }
+    void asignarTipo(String tipo){
+
+    }
+}
